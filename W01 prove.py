3a1fc1b2317caa195784fcd7bbfad97189436288
CBell045 <chadbell045@gmail.com>
2022-01-12 21:26:13 -0700

    Update W01 prove.py

--- W01 prove.py
+++ W01 prove.py
@@ -6,37 +6,39 @@ Assignment: Week 01 Prove
 
 def main():
     board = [i for i in range(1, 10)]
+    print("\n\n\nIt's player X's turn")
     print_board(board)
     is_finished = False
     turn = "X"
     while is_finished == False:
         if turn == "X":
             board[user_move()-1] = "X"
+            print("\n\n\nIt's player O's turn")
             turn = "0"
         else:
             board[user_move()-1] = "O"
+            print("\n\n\nIt's player X's turn")
             turn = "X"
-        print(board[0:3], board[0:3][::-1], board[0:3]==board[0:3][::-1])
         print_board(board)
         if board[0:3]==board[0:3][::-1]:
-            is_finished == True
+            is_finished = True
         elif board[3:6]==board[3:6][::-1]:
-            is_finished == True
+            is_finished = True
         elif board[6:9]==board[6:9][::-1]:
-            is_finished == True
+            is_finished = True
         elif board[0]==board[3]==board[6]:
-            is_finished == True
+            is_finished = True
         elif board[1]==board[4]==board[7]:
-            is_finished == True
+            is_finished = True
         elif board[2]==board[5]==board[8]:
-            is_finished == True
+            is_finished = True
         elif board[0]==board[4]==board[8]:
-            is_finished == True
+            is_finished = True
         elif board[2]==board[4]==board[6]:
-            is_finished == True
+            is_finished = True
         else:
             next
-    print("You Won!")
+    print(f"You Won!")
             
 
 def print_board(board):
